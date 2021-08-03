1bf870064068bf1192382ee2748dbe6a08edf45c
Spotlight <spotlight@joscomputing.space>
2021-08-03 16:05:03 -0500

    Use mxml accessor functions
    It appears struct-based access of values was deprecated a long time ago.

--- xml.c
+++ xml.c
@@ -32,44 +32,42 @@ get_value(mxml_node_t *node,		/* I - Node to get */
 	ptr = (char*)buffer;
 	end = (char*)buffer + buflen - 1;
 	char tempbuf[4092];
-	current = node->child;
-	for (current = node->child; current && ptr < end; current = current->next)
-	{
-		if (current->type == MXML_TEXT)
-		{
-			if (current->value.text.whitespace)
+
+	for (current = mxmlGetNextSibling(node); current && ptr < end; current = mxmlGetNextSibling(current)) {
+		mxml_type_t currentType = mxmlGetType(current);
+		if (currentType == MXML_TEXT) {
+			int whitespace;			
+			const char* value = mxmlGetText(current, &whitespace);
+			if (whitespace)
 				*ptr++ = ' ';
 
-			len = (int)strlen(current->value.text.string);
+			len = strlen(value);
 			if (len > (int)(end - ptr))
 				len = (int)(end - ptr);
 
-			memcpy(ptr, current->value.text.string, len);
+			memcpy(ptr, value, len);
 			ptr += len;
-		}
-		else if (current->type == MXML_OPAQUE)
-		{
-			len = (int)strlen(current->value.opaque);
+		} else if (currentType == MXML_OPAQUE) {
+			const char* value = mxmlGetOpaque(current);
+			len = strlen(value);
 			if (len > (int)(end - ptr))
 				len = (int)(end - ptr);
 
-			memcpy(ptr, current->value.opaque, len);
+			memcpy(ptr, value, len);
 			ptr += len;
 		}
-		else if (current->type == MXML_INTEGER)
+		else if (currentType == MXML_INTEGER)
 		{
-			sprintf(tempbuf, "%d", current->value.integer);
-			len = (int)strlen(tempbuf);
+			sprintf(tempbuf, "%d", mxmlGetInteger(current));
+			len = strlen(tempbuf);
 			if (len > (int)(end - ptr))
 				len = (int)(end - ptr);
 
 			memcpy(ptr, tempbuf, len);
 			ptr += len;
-		}
-		else if (current->type == MXML_REAL)
-		{
-			sprintf(tempbuf, "%f", current->value.real);
-			len = (int)strlen(tempbuf);
+		} else if (currentType == MXML_REAL) {
+			sprintf(tempbuf, "%f", mxmlGetReal(current));
+			len = strlen(tempbuf);
 			if (len > (int)(end - ptr))
 				len = (int)(end - ptr);
             
@@ -84,7 +82,7 @@ get_value(mxml_node_t *node,		/* I - Node to get */
 const char *whitespace_cb(mxml_node_t *node, int where)
 {
 	/* code by Matt_P */
-	const char *name = node->value.element.name;
+	const char *name = mxmlGetElement(node);
 	mxml_node_t * temp = node;
 	char tab_buffer[25];
 	memset( tab_buffer , 0 , 15 );
@@ -92,15 +90,14 @@ const char *whitespace_cb(mxml_node_t *node, int where)
 	while(temp){
 		depth++;
 		tab_buffer[depth - 1] = '\t';
-		temp = temp->parent;
+		temp = mxmlGetParent(temp);
 	}
 	if (where == 3 || where == 1){
 		return "";
-	}else if(((node->prev && where == 0) || node->parent) && !(where == 2 && strcmp(name, "rotate") && strcmp(name, "scale") && strcmp(name, "translate") && strcmp(name, "xmlyt") && strcmp(name, "xmlan") && strcmp(name, "pai1") && strcmp(name, "pat1") && strcmp(name, "pah1") && strcmp(name, "seconds") && strcmp(name, "entries") && strcmp(name, "triplet") && strcmp(name, "pair") && strcmp(name, "entry") && strcmp(name, "pane") && (strcmp(name, "tag") && strcmp(name, "size") && strcmp(name, "material") && strcmp(name, "vtx") && strcmp(name, "colors") && strcmp(name, "subs") && strcmp(name, "usdentry") && strcmp(name, "font") && strcmp(name, "wnd4") && strcmp(name, "wnd4mat") && strcmp(name, "set") && strcmp(name, "coordinates") && strcmp(name, "TevStage") && strcmp(name, "texture") && strcmp(name, "TextureSRT") && strcmp(name, "CoordGen") && strcmp(name, "ChanControl") && strcmp(name, "MaterialColor") && strcmp(name, "TevSwapModeTable") && strcmp(name, "IndTextureSRT") && strcmp(name, "IndTextureOrder") && strcmp(name, "AlphaCompare") && strcmp(name, "BlendMode") )) ){
+	}else if(((mxmlGetPrevSibling(node) && where == 0) || mxmlGetParent(node)) && !(where == 2 && strcmp(name, "rotate") && strcmp(name, "scale") && strcmp(name, "translate") && strcmp(name, "xmlyt") && strcmp(name, "xmlan") && strcmp(name, "pai1") && strcmp(name, "pat1") && strcmp(name, "pah1") && strcmp(name, "seconds") && strcmp(name, "entries") && strcmp(name, "triplet") && strcmp(name, "pair") && strcmp(name, "entry") && strcmp(name, "pane") && (strcmp(name, "tag") && strcmp(name, "size") && strcmp(name, "material") && strcmp(name, "vtx") && strcmp(name, "colors") && strcmp(name, "subs") && strcmp(name, "usdentry") && strcmp(name, "font") && strcmp(name, "wnd4") && strcmp(name, "wnd4mat") && strcmp(name, "set") && strcmp(name, "coordinates") && strcmp(name, "TevStage") && strcmp(name, "texture") && strcmp(name, "TextureSRT") && strcmp(name, "CoordGen") && strcmp(name, "ChanControl") && strcmp(name, "MaterialColor") && strcmp(name, "TevSwapModeTable") && strcmp(name, "IndTextureSRT") && strcmp(name, "IndTextureOrder") && strcmp(name, "AlphaCompare") && strcmp(name, "BlendMode") )) ){
 		sprintf( xmlbuff , "\n%s" , tab_buffer );
 	}else{
 		return "";
 	}
 	return xmlbuff;
 }
-
